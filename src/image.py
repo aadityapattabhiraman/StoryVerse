bbc3f3989a75372669c8b2c2acea1ba451ebb416
aadityapattabhiraman <154038229+aadityapattabhiraman@users.noreply.github.com>
2025-06-02 06:34:44 +0000

    renamed files, added situations

---
+++ src/image.py
@@ -0,0 +1,48 @@
+import os
+import time
+import base64
+import json
+from typing import List
+from datetime import datetime, timedelta
+from openai import OpenAI
+
+
+def get_images(directory: str) -> List[str]:
+
+    images = []
+    for image in os.listdir(directory):
+
+        if (image.endswith(".png")) or (image.endswith(".jpg")):
+            images.append(image)
+
+    return images
+
+
+def get_prompt(path: str) -> str:
+
+    print(path)
+    prompt = """"""
+
+    return prompt
+
+
+def get_situation(path: str) -> List:
+
+    with open(path) as f:
+        data = json.load(f)
+
+    return data
+
+
+def pipeline():
+
+    story = "../data/Maya and the Little Flower/"
+    template_images = get_images(story)
+    print(template_images)
+    situations = get_situation(f"{story}story.json")
+    print(situations)
+
+
+if __name__ == "__main__":
+
+    pipeline()
